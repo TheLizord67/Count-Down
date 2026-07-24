a249295700fb1943de84f3a4f261851f1f76b6d1
tobiahsday@gmail.com <tobiahsday@gmail.com>
2026-07-24 13:21:52 -0700

    lag fix

--- Assets/Toby/Scripts/Enemy/EnemyController.cs
+++ Assets/Toby/Scripts/Enemy/EnemyController.cs
@@ -55,7 +55,7 @@ public class EnemyController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        AstarPath.active.Scan();
+        //AstarPath.active.Scan();
         attackStyle = attackStylesList[Random.Range(0, attackStylesList.Length)];
         GameObject[] points = GameObject.FindGameObjectsWithTag("Retreat");
         foreach (var point in points)
